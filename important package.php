5fdb44485e8aaaf7ead38b3bc458219a9b3cbb55
surujmiah6896 <surujmiah6896@gmail.com>
2024-01-03 10:56:17 +0600

    init git

---
+++ important package.php
@@ -0,0 +1,41 @@
+#default image package
+https://github.com/laravolt/avatar
+
+#noCaptcha google
+https://github.com/anhskohbo/no-captcha
+
+#number to word generator
+https://github.com/kwn/number-to-words
+
+#image upload to directorty
+http://image.intervention.io/
+
+#laravel charts
+https://erik.cat/projects/charts
+
+#pdf download
+https://github.com/barryvdh/laravel-dompdf
+
+#permission package
+https://github.com/spatie/laravel-permission
+
+#breadcrumbs generator
+https://github.com/davejamesmiller/laravel-breadcrumbs
+
+#add make:view command at cli
+https://github.com/svenluijten/artisan-view
+
+#export excel
+https://github.com/Maatwebsite/Laravel-Excel
+
+#barcode/qr code generator
+https://github.com/milon/barcode
+
+#database backup for 5.5 or less
+https://github.com/spatie/laravel-backup
+
+#tons of package by spatie
+https://spatie.be/en/opensource/laravel
+
+#package for Bkash integration
+https://github.com/karim-007/laravel-bkash-tokenize
